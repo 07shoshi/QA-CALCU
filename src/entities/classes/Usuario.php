5244bd68a81fa4fba901322906f30c574be6b4ec
allanmendezema <102200062+allanmendezema@users.noreply.github.com>
2025-08-23 11:25:41 -0600

    Update Usuario.php
    creando clase usuario

---
+++ src/entities/classes/Usuario.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Entities\Classes;
+
+use App\Entities\Interfaces\IUsuario;
+
+class Usuario implements IUsuario {
+    private $nombre;
+    private $id;
+    private $preferencias = [];
+
+    public function __construct(string $nombre, int $id) {
+        $this->nombre = $nombre;
+        $this->id = $id;
+    }
+
+    public function getNombre(): string {
+        return $this->nombre;
+    }
+
+    public function getId(): int {
+        return $this->id;
+    }
+
+    public function setPreferencias(array $prefs): void {
+        $this->preferencias = $prefs;
+    }
+}
