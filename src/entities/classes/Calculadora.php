e613ecd514514dc0107f1d29ef70c93d3eac4540
lucasJosue <94641368+dragonz124@users.noreply.github.com>
2025-08-23 11:18:39 -0600

    Update Calculadora.php
    Creando la clase para la calculadora

--- src/entities/classes/Calculadora.php
+++ src/entities/classes/Calculadora.php
@@ -1,9 +1,23 @@
 <?php
-namespace App\Entities\Interfaces;
+namespace App\Entities\Classes;
 
-interface InterfaceCalculadora {
-    public function sumar(float $a, float $b): float;
-    public function restar(float $a, float $b): float;
-    public function multiplicar(float $a, float $b): float;
-    public function dividir(float $a, float $b): float;
+use App\Entities\Interfaces\InterfaceCalculadora;
+
+class Calculadora implements ICalculadora {
+    public function sumar(float $a, float $b): float {
+        return $a + $b;
+    }
+
+    public function restar(float $a, float $b): float {
+        return $a - $b;
+    }
+
+    public function multiplicar(float $a, float $b): float {
+        return $a * $b;
+    }
+
+    public function dividir(float $a, float $b): float {
+        if ($b == 0) throw new \Exception("División por cero");
+        return $a / $b;
+    }
 }
